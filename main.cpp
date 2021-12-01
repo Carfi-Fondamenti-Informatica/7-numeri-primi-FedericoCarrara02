3e786b23f36d3489217a06d32fc01fa724482971
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-12-01 19:09:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using namespace std;
+#include "lib_nprimi.h"
+#include <string>
 
-int main(){
-  return 0;
+int main() {
+    int i = 0;
+    int p;
+    string b = "False";
+    cin >> i;
+    p = i-1;
+
+    string check = n_primo(i, p, b);
+    if (check.compare("Vero") == 0) {
+        cout << "numero primo" << endl;
+    }
+    else {
+        cout << "numero non primo" << endl;
+    }
+
+    return 0;
 }
+
