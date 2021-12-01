1c949548971cef8fd01991bd005219b433f2631e
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-12-01 19:15:18 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+#ifndef ESERCITAZIONE_VALUTATA_7_LIB_NPRIMI_H
+#define ESERCITAZIONE_VALUTATA_7_LIB_NPRIMI_H
+#include <string>
+using namespace std;
 
+string n_primo (int a, int p, string b);
+
+#endif //ESERCITAZIONE_VALUTATA_7_LIB_NPRIMI_H
