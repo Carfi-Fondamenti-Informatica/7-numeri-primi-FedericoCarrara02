8d743f668a93f4588bc909babeb7f45b9bba63fd
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-12-01 19:14:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+#include "lib.h"
+#include <string>
+using namespace std;
+
+string n_primo (int a, int p, string b) {
+    if (a < 2) {
+        return "False";
+    }
+
+    int resto = a%p;
+        if (resto != 0) {
+            p--;
+            return n_primo(a, p, b);
+        }
+        else {
+            if (p == 1) {
+                return "Vero";
+            } else {
+                return "False";
+            }
+        }
+}
 
